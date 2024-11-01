ee51e0abbdf3d97f7c03f73e309c0d8b04041704
dosconio <m360xc@outlook.com>
2024-11-01 18:42:03 +0800

    2024ATM CH4 CMT0

--- os/src/syscall/process.rs
+++ os/src/syscall/process.rs
@@ -1,9 +1,16 @@
 //! Process management syscalls
+#![deny(warnings)]
+
+use core::mem::size_of;
 use crate::{
     config::MAX_SYSCALL_NUM,
     task::{
         change_program_brk, exit_current_and_run_next, suspend_current_and_run_next, TaskStatus,
     },
+
+	mm::translated_byte_buffer, task::{
+        current_user_token, get_start_time, get_syscall_times, do_task_mmap, do_task_munmap
+    }, timer::{/*get_time,*/ get_time_ms, get_time_us}
 };
 
 #[repr(C)]
@@ -38,32 +45,71 @@ pub fn sys_yield() -> isize {
     0
 }
 
-/// YOUR JOB: get time with second and microsecond
-/// HINT: You might reimplement it with virtual memory management.
-/// HINT: What if [`TimeVal`] is splitted by two pages ?
+/// 
 pub fn sys_get_time(_ts: *mut TimeVal, _tz: usize) -> isize {
     trace!("kernel: sys_get_time");
-    -1
+    let us = get_time_us();
+    let time_val = TimeVal {
+        sec: us / 1_000_000,
+        usec: us % 1_000_000,
+    };
+    let buffers = translated_byte_buffer(current_user_token(), _ts as *const u8, size_of::<TimeVal>());
+    let mut time_val_ptr = &time_val as *const _ as *const u8;
+    for buffer in buffers {
+        unsafe {
+            time_val_ptr.copy_to(buffer.as_mut_ptr(), buffer.len());
+            time_val_ptr = time_val_ptr.add(buffer.len());
+        }
+    }
+    0
 }
 
-/// YOUR JOB: Finish sys_task_info to pass testcases
-/// HINT: You might reimplement it with virtual memory management.
-/// HINT: What if [`TaskInfo`] is splitted by two pages ?
+/// 
 pub fn sys_task_info(_ti: *mut TaskInfo) -> isize {
-    trace!("kernel: sys_task_info NOT IMPLEMENTED YET!");
-    -1
+    trace!("kernel: sys_task_info");
+    let mut tmp = TaskInfo {
+        time : get_time_ms() - (get_start_time() as usize),
+        syscall_times: [0; 500],
+        status : TaskStatus::Running,
+    };
+    get_syscall_times(&mut tmp.syscall_times);
+    let buffers = translated_byte_buffer(current_user_token(), _ti as *const u8, size_of::<TaskInfo>());
+    let mut task_info_ptr = &tmp as *const _ as *const u8;
+    for buffer in buffers {
+        unsafe {
+            task_info_ptr.copy_to(buffer.as_mut_ptr(), buffer.len());
+            task_info_ptr = task_info_ptr.add(buffer.len());
+        }
+    }
+    0
 }
 
-// YOUR JOB: Implement mmap.
+///
 pub fn sys_mmap(_start: usize, _len: usize, _port: usize) -> isize {
-    trace!("kernel: sys_mmap NOT IMPLEMENTED YET!");
-    -1
+    trace!("kernel: sys_mmap");
+    // trace!("kernel: sys_mmap {:#x} ~ {:#x}", _start, _start + _len);
+    
+    if _port & 0x7 == 0 {
+        return -1;// Useless Mapping Page
+    }
+    if _start & 0xfff != 0 || _port & !(0b111 as usize) != 0{
+        return -1;// Tutorial Request
+    }
+    if _len == 0 {
+        return 0;//{ISSUE}
+    }
+
+    if do_task_mmap(_start, _len, _port) { 0 } else { -1 }
 }
 
-// YOUR JOB: Implement munmap.
+///
 pub fn sys_munmap(_start: usize, _len: usize) -> isize {
-    trace!("kernel: sys_munmap NOT IMPLEMENTED YET!");
-    -1
+    trace!("kernel: sys_munmap");
+    if _len == 0 {
+        return 0;//{ISSUE}
+    }
+    //info!(">>>>>>>>>{:#x}!!{:#x}", _start, _start + _len);// any info/error! here will stuck system
+    if do_task_munmap(_start, _len) { 0 } else { -1 }
 }
 /// change data segment size
 pub fn sys_sbrk(size: i32) -> isize {
